b07cd213526371ed5ae7adbba15fe4cbb9f98408
Jordan Powell <jpowproductions@gmail.com>
2025-07-03 14:45:08 -0500

    Disabled form routes and added a url to the register button

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Admin from './pages/Admin'
 import Home from './pages/Home'
-import Contact from './pages/Contact'
-import Register from './pages/Register'
 import NoPage from './pages/NoPage'
 
 function App() {
@@ -14,9 +12,6 @@ function App() {
           {/* <Route index element={<Home />} /> */}
           <Route path='/' element={<Home />} />
           <Route path='/home' element={<Home />} />
-          <Route path='/contact' element={<Contact />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/rpowell' element={<Admin />} />
           <Route path='*' element={<NoPage />} />
         </Routes>
       </BrowserRouter>
